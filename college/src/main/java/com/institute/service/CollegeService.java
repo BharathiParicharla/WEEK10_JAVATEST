524fbcb65513993a6a7ca52f416b59c3499b3289
BharathiParicharla <152381122+BharathiParicharla@users.noreply.github.com>
2024-01-11 18:19:12 +0530

    Delete college directory

--- college/src/main/java/com/institute/service/CollegeService.java
+++
@@ -1,9 +0,0 @@
-package com.institute.service;
-
-import org.springframework.stereotype.Service;
-
-import com.institute.bean.CollegeBean;
-
-public interface CollegeService {
-	void save(CollegeBean collegeBean);
-}
